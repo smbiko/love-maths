07fe0e841e4e19d5d730e38197305742e925eee0
Siyasanga Mbiko <smbiko@gmail.com>
2024-03-25 18:37:21 +0000

    Fixed typo error on line 61 of the script js

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,7 @@ function displayAdditionQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+git ";
+    document.getElementById('operator').textContent = "+";
 
 }
 
